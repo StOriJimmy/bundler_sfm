b795220b6a9150f0cb07929577daadeca05e34ff
StOriJimmy <liuxy0319@outlook.com>
2020-04-18 23:05:45 +0800

    vs2017 build x64 fix

--- include/types.h
+++ include/types.h
@@ -3,8 +3,9 @@
 typedef unsigned char u_int8_t;
 typedef unsigned short u_int16_t;
 typedef unsigned long u_int32_t;
-//typedef signed long int32_t;
-
+#ifndef WIN32
+typedef signed long int32_t;
+#endif
 typedef signed short int16_t;
 
 #endif /* __my_types_h__ */
